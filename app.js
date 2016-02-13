09eea6ff4edb6a744e4d347a55b4631e6f4146b7
Jake <jake@jakebladt.com>
2016-02-13 15:46:52 -0500

    +home path

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ var jade = require('jade');
 var app = express();
 app.set('port', (process.env.PORT || 8888));
 app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
 
+app.get("/", function(req, res) {
+  res.render("home", {
+    pageTitle: "Walker"
+  });
+});
 
 app.listen(app.get('port'), function() {
   console.log('Walker running on port', app.get('port'));
