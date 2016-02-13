61f80b6fb4f32748cbc2e9219159a810c369a110
Jake <jake@jakebladt.com>
2016-02-13 15:41:05 -0500

    +home view

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var jade = require('jade');
 
 var app = express();
 app.set('port', (process.env.PORT || 8888));
+app.set('view engine', 'jade');
 
 
 app.listen(app.get('port'), function() {
